d4ea05a74c9d6e2297991d2823cc7d62e0b930b4
Parth Garg <parthgarg750@gmail.com>
2025-11-10 10:32:14 +0530

    Arrays

---
+++ 40_move_zeroes.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int tracker = 0;
+        int n = nums.size();
+        for(int i = 0 ; i < n; i++){
+            if ( nums[i] != 0){
+                swap(nums[i],nums[tracker]);
+                tracker++;
+            }
+        }
+    }
+};
