c717b518df1a943378080603c2ba1ea1247f038c
Parth Garg <parthgarg750@gmail.com>
2025-11-11 23:49:08 +0530

    Array

---
+++ 41_Remove_duplicate_array.cpp
@@ -0,0 +1,23 @@
+// #include <algorithm>
+// #include <unordered_map>
+// #include <vector>
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& arr) {
+
+//     int n = arr.size();
+//     int k = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (arr[i] != arr[k])
+//         {
+//             swap(arr[i], arr[k + 1]);
+//             k += 1;
+//         }
+//     }
+
+//     return k + 1;
+//     }
+// };
