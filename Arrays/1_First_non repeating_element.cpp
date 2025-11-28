8c0f6ae4e837e3b3062cf69f24cfda4b761165cd
Parth Garg <parthgarg750@gmail.com>
2025-11-28 12:24:59 +0530

    Find the 1st non repeating element in the array

---
+++ Arrays/1_First_non repeating_element.cpp
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/non-repeating-element3958/1
+
+// #include <iostream>
+// #include <unordered_map>
+
+// using namespace std;
+
+// class Solution {
+//   public:
+//     int firstNonRepeating(vector<int>& arr) {
+//         unordered_map<int , int> map ;
+//         for(auto x : arr)
+//             map[x]++;
+            
+//         for(auto x: arr)
+//         {
+            
+//             if(map[x] == 1)
+//              return x;
+//         }
+//         return 0;
+        
+//     }
+// };
