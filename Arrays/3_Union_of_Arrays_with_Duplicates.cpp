6902ed31cf9b9b411116ea5a480f768bbf654bd7
Parth Garg <parthgarg750@gmail.com>
2025-12-01 08:23:08 +0530

    Array

---
+++ Arrays/3_Union_of_Arrays_with_Duplicates.cpp
@@ -0,0 +1,29 @@
+// https://www.geeksforgeeks.org/problems/union-of-two-arrays3538/1
+
+// #include <iostream>
+// #include <vector>
+// #include <unordered_map>
+
+// using namespace std;
+
+// class Solution
+// {
+// public:
+//   vector<int> findUnion(vector<int> &a, vector<int> &b)
+//   {
+
+//     unordered_map<int, int> freq;
+//     vector<int> v;
+
+//     for (auto x : a)
+//       freq[x] += 1;
+
+//     for (auto x : b)
+//       freq[x] += 1;
+
+//     for (auto &p : freq)
+//       v.push_back(p.first);
+
+//     return v;
+//   }
+// };
