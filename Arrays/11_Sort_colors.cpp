e31d476c738d4ec9b4f29f8a43df38a0c9a842bc
Parth Garg <parthgarg750@gmail.com>
2025-12-02 08:11:53 +0530

    array

---
+++ Arrays/11_Sort_colors.cpp
@@ -0,0 +1,37 @@
+// https : // leetcode.com/problems/sort-colors/description/
+
+//         class Solution
+// {
+// public:
+//     void sortColors(vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         int c1 = 0, c2 = 0, c3 = 0;
+//         for (int i = 0; i < n; i++)
+//         {
+//             if (nums[i] == 0)
+//                 c1 += 1;
+//             if (nums[i] == 1)
+//                 c2 += 1;
+//             if (nums[i] == 2)
+//                 c3 += 1;
+//         }
+
+//         int i = 0;
+
+//         while (c1--)
+//         {
+//             nums[i++] = 0;
+//         }
+
+//         while (c2--)
+//         {
+//             nums[i++] = 1;
+//         }
+
+//         while (c3--)
+//         {
+//             nums[i++] = 2;
+//         }
+//     }
+// };
